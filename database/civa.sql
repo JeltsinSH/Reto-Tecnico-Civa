d6154ff2126a26a1e7fbace24bc7f1f4327195ce
JeltsinSH <75293599+JeltsinSH@users.noreply.github.com>
2025-04-09 16:49:30 -0500

    Agregando base de datos

--- database/civa.sql
+++ database/civa.sql
@@ -1,1 +1,114 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-04-2025 a las 23:40:28
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `civa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bus`
+--
+
+CREATE TABLE `bus` (
+  `id` bigint(20) NOT NULL,
+  `numero_bus` varchar(255) DEFAULT NULL,
+  `placa` varchar(255) DEFAULT NULL,
+  `fecha_creacion` date DEFAULT NULL,
+  `caracteristicas` varchar(255) DEFAULT NULL,
+  `activo` bit(1) NOT NULL,
+  `marca_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `bus`
+--
+
+INSERT INTO `bus` (`id`, `numero_bus`, `placa`, `fecha_creacion`, `caracteristicas`, `activo`, `marca_id`) VALUES
+(1, '1001', 'ABC-123', '2005-05-01', 'Bus premium', b'1', 1),
+(2, '1002', 'XYZ-456', '2020-01-15', 'Bus económico', b'0', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca`
+--
+
+CREATE TABLE `marca` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `marca`
+--
+
+INSERT INTO `marca` (`id`, `nombre`) VALUES
+(1, 'Volvo'),
+(2, 'Scania'),
+(3, 'Fiat');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bus`
+--
+ALTER TABLE `bus`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `marca_id` (`marca_id`);
+
+--
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bus`
+--
+ALTER TABLE `bus`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `bus`
+--
+ALTER TABLE `bus`
+  ADD CONSTRAINT `bus_ibfk_1` FOREIGN KEY (`marca_id`) REFERENCES `marca` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
