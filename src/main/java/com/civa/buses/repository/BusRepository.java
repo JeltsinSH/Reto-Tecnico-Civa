7df8eab89b334b6682403f0f398b87a52501af14
JeltsinSH <75293599+JeltsinSH@users.noreply.github.com>
2025-04-09 16:46:47 -0500

    Delete src directory

--- src/main/java/com/civa/buses/repository/BusRepository.java
+++
@@ -1,8 +0,0 @@
-package com.civa.buses.repository;
-
-import com.civa.buses.model.Bus;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusRepository extends JpaRepository<Bus, Long> {
-	
-}
